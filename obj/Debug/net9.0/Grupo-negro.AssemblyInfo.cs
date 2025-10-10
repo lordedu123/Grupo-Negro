bb0c40858851a22ee767d27055a47a1a367f5a03
lordedu <zangetsu45533@gmail.com>
2025-10-10 02:39:07 -0500

    Implementando validaciones JavaScript para formularios

--- obj/Debug/net9.0/Grupo-negro.AssemblyInfo.cs
+++ obj/Debug/net9.0/Grupo-negro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grupo-negro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfd769465fc9a2bc3d982cedacb005e3b8cf67c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba1510bbf1b5045b608d3af8cd8149aeb1d41a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grupo-negro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grupo-negro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/js/validaciones.js
@@ -0,0 +1,16 @@
+// Validaciones adicionales para formularios
+function validarFormulario(form) {
+    const inputs = form.querySelectorAll('input[required]');
+    let isValid = true;
+    
+    inputs.forEach(input => {
+        if (!input.value.trim()) {
+            isValid = false;
+            input.classList.add('error');
+        } else {
+            input.classList.remove('error');
+        }
+    });
+    
+    return isValid;
+}
